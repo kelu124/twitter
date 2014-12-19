6a9f111311ae6415b8213b2f8e656f8d235a8f68
KG <admin@echopen.org>
2014-12-19 20:36:46 +0100

    19-12-2014 Automatic

--- t-graph.py
+++ t-graph.py
@@ -24,32 +24,48 @@ result = cursor.fetchall()
 
 # let's clean the data 
 paireds = []
+tHList = []
 tUsers = []
+tHashes = []
 i = 0
 for row in result:
 	# for bugbs in my mysql dump due to bad retweets
 	 
-	usered = row[0]
+	usered = row[0].lower()
 	tmp = string.replace(row[1], 'RT ', 'RT @') 
 	tmp = string.replace(tmp, '@@', '@')
+	tmp = tmp.lower()
+	users = []
 	users = re.findall("@\w+", tmp)
-	users.append (usered)
+	
+	tHashers = re.findall("#\w+", tmp)
+	users.append(usered)
+	
+	users = users + tHashers 
+	tHashes.append(tHashers)
 	#print users
 	paireds.append(users)
+	#paireds.append(tHashers)
+
 	i = i +1 
 	tUsers.append(usered)
+	tHList = tHList + tHashers
 
 #print paireds
 #print tUsers
 tUserUnique = []
 [tUserUnique.append(item) for item in tUsers if item not in tUserUnique]
+tH = []
+[tH.append(item) for item in tHList if item not in tH]
 #print tUserUnique
 
-print 'nodedef>name VARCHAR,label VARCHAR'
+print 'nodedef> name VARCHAR,label VARCHAR'
 for user in tUserUnique:
 	print "%s, %s" % (user, user)
+for user in tH:
+	print "%s, %s" % (user, user)
 
-print 'edgedef>node1 VARCHAR,node2 VARCHAR'
+print 'edgedef> node1 VARCHAR,node2 VARCHAR'
 for seque in paireds:
 	l = len(seque)
 	#print seque
